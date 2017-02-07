c1e6ff3287d250f70dbc0fed6d821dd0c77d13ae
Bryce Kahle <bkahle@gmail.com>
2017-02-07 11:09:36 -0500

    Add v2 format config for older projects

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
 module.exports = {
 	"extends": "eslint:recommended",
 	"rules": {
-		"array-bracket-spacing": ["error", "never"],
-		"block-scoped-var": "error",
-		"brace-style": ["error", "1tbs"],
-		"camelcase": ["error", { "properties": "never" }],
-		"constructor-super": "error",
-		"curly": "error",
-		"eol-last": "error",
-		"eqeqeq": ["error", "smart"],
-		"func-names": "error",
-		"func-style": ["error", "declaration", { "allowArrowFunctions": true }],
-		"handle-callback-err": "error",
-		"indent": ["error", "tab", {
-			"SwitchCase": 1,
-			"ObjectExpression": "first",
-		  "MemberExpression": 1
+		"array-bracket-spacing": [2, "never"],
+		"block-scoped-var": 2,
+		"brace-style": [2, "1tbs"],
+		"camelcase": [2, { "properties": "never" }],
+		"constructor-super": 2,
+		"curly": 2,
+		"eol-last": 2,
+		"eqeqeq": [2, "smart"],
+		"func-names": 2,
+		"func-style": [2, "declaration", { "allowArrowFunctions": true }],
+		"handle-callback-err": 2,
+		"indent": [2, "tab", {
+			"SwitchCase": 1
 		}],
-		"keyword-spacing": ["error", {
+		"keyword-spacing": [2, {
 		  "before": true,
 		  "after": true,
 		}],
-		"max-depth": ["warn", 3],
-		"max-statements": ["warn", 30],
-		"new-cap": "warn",
-		"no-class-assign": "error",
-		"no-cond-assign": ["error", "except-parens"],
-		"no-const-assign": "error",
-		"no-dupe-class-members": "error",
-		"no-extend-native": "error",
-		"no-extra-parens": ["error", "functions"],
-		"no-mixed-spaces-and-tabs": "error",
-		"no-multi-spaces": ["warn", {
+		"max-depth": [1, 3],
+		"max-statements": [1, 30],
+		"new-cap": 1,
+		"no-class-assign": 2,
+		"no-cond-assign": [2, "except-parens"],
+		"no-const-assign": 2,
+		"no-dupe-class-members": 2,
+		"no-extend-native": 2,
+		"no-extra-parens": [2, "functions"],
+		"no-mixed-spaces-and-tabs": 2,
+		"no-multi-spaces": [1, {
 			"exceptions": {
 				"VariableDeclarator": true
 			}
 		}],
-		"no-nested-ternary": "error",
-		"no-this-before-super": "error",
-		"no-trailing-spaces": "error",
-		"no-undef": "error",
-		"no-underscore-dangle": "off",
-		"no-unused-vars": ["warn", { "vars": "all", "args": "none"}],
-		"no-use-before-define": ["error", "nofunc"],
-		"object-curly-spacing": ["error", "always"],
-		"quotes": ["error", "single", "avoid-escape"],
-		"semi": ["error", "always"],
-		"space-unary-ops": ["error", {"words": true, "nonwords": false}],
-		"strict": ["error", "global"],
-		"valid-jsdoc": ["warn", {
+		"no-nested-ternary": 2,
+		"no-this-before-super": 2,
+		"no-trailing-spaces": 2,
+		"no-undef": 2,
+		"no-underscore-dangle": 0,
+		"no-unused-vars": [1, { "vars": "all", "args": "none"}],
+		"no-use-before-define": [2, "nofunc"],
+		"object-curly-spacing": [2, "always"],
+		"quotes": [2, "single", "avoid-escape"],
+		"semi": [2, "always"],
+		"space-unary-ops": [2, {"words": true, "nonwords": false}],
+		"strict": [2, "global"],
+		"valid-jsdoc": [1, {
 			"requireReturn": false,
 			"requireParamDescription": false
 		}]
