ef66fd1238c15ac6494121a4764ee6fe634b9977
Cole Abbeduto <cole.abbeduto@particle.io>
2025-10-21 11:00:04 -0500

    Move help info for tests into README

--- test/src/index.js
+++ test/src/index.js
@@ -1,22 +1,6 @@
 'use strict';
 /* eslint no-unused-vars: 'off' */
 
-/**
- * To add a test, make a new function with code that should or should not be allowed.
- *
- * Code that should not be allowed should be commented with the eslint disable directive above it.
- * If the specified rule is not broken, eslint will error with an "unused directive" error which
- * enforces correctness for our "should not" tests.
- *
- * ```javascript
- * codeThatShouldBeAllowed()
- *
- * // eslint-disable-next-line rule-that-is-broken
- * codeThatBreaksTheRule()
- * ```
- */
-
-
 function preferConstDestructing() {
 	// Should allow let usage on destructuring when only one variable is reassigned
 	let { a, b } = { a: 0, b: 100 };
